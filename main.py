14c0af07abf85f22e02db0c85575080756b30aa7
José Duque <duque.castiblanco@gmail.com>
2026-09-20 12:11:10 -0500

    "Agrego comentario descriptivo"

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ from database import engine, Base, get_db
 from typing import Optional
 
 from modelos import Producto, ProductoDB, UsuarioDB, UsuarioRegistro
-from auth import hashear_password, verificar_password, crear_token, obtener_usuario_actual
+from auth import (
+    hashear_password,
+    verificar_password,
+    crear_token,
+    obtener_usuario_actual,
+)
 
 from fastapi.security import OAuth2PasswordRequestForm
 
@@ -14,16 +19,18 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-#simulación de la base de datos
+# simulación de la base de datos
 inventario = []
-#variable id
-contador_id= 1
+# variable id
+contador_id = 1
+
+
+# endpoint de prueba
 
 
 @app.get("/")
 def inicio():
     return {"mensaje": "Api de inventario en proceso de desarrollo"}
-
 
 
 @app.get("/productos/{producto_id}")
@@ -33,28 +40,40 @@ def obtener_producto(producto_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Producto no encontrado")
     return item
 
+
 @app.get("/productos")
-def listar_productos(nombre: Optional[str] = None , db: Session = Depends(get_db)):
+def listar_productos(nombre: Optional[str] = None, db: Session = Depends(get_db)):
     query = db.query(ProductoDB)
     if nombre:
         query = query.filter(ProductoDB.nombre.ilike(f"%{nombre}%"))
     return query.all()
 
+
 @app.post("/productos")
-def crear_producto(producto: Producto, db: Session = Depends(get_db), usuario: str = Depends(obtener_usuario_actual)):
+def crear_producto(
+    producto: Producto,
+    db: Session = Depends(get_db),
+    usuario: str = Depends(obtener_usuario_actual),
+):
     nuevo_producto = ProductoDB(
         nombre=producto.nombre,
         descripcion=producto.descripcion,
         precio=producto.precio,
-        cantidad_disponible=producto.cantidad_disponible
+        cantidad_disponible=producto.cantidad_disponible,
     )
     db.add(nuevo_producto)
     db.commit()
     db.refresh(nuevo_producto)
     return {"mensaje": "Producto creado", "producto": nuevo_producto}
 
+
 @app.put("/productos/{producto_id}")
-def actualizar_producto(producto_id: int, producto: Producto, db: Session = Depends(get_db), usuario: str = Depends(obtener_usuario_actual)):
+def actualizar_producto(
+    producto_id: int,
+    producto: Producto,
+    db: Session = Depends(get_db),
+    usuario: str = Depends(obtener_usuario_actual),
+):
     item = db.query(ProductoDB).filter(ProductoDB.id == producto_id).first()
     if item is None:
         raise HTTPException(status_code=404, detail="Producto no encontrado")
@@ -68,7 +87,11 @@ def actualizar_producto(producto_id: int, producto: Producto, db: Session = Depe
 
 
 @app.delete("/productos/{producto_id}")
-def eliminar_producto(producto_id: int, db: Session = Depends(get_db), usuario: str = Depends(obtener_usuario_actual)):
+def eliminar_producto(
+    producto_id: int,
+    db: Session = Depends(get_db),
+    usuario: str = Depends(obtener_usuario_actual),
+):
     item = db.query(ProductoDB).filter(ProductoDB.id == producto_id).first()
     if item is None:
         raise HTTPException(status_code=404, detail="Producto no encontrado")
@@ -79,24 +102,32 @@ def eliminar_producto(producto_id: int, db: Session = Depends(get_db), usuario: 
 
 @app.post("/usuarios")
 def registrar_usuario(usuario: UsuarioRegistro, db: Session = Depends(get_db)):
-    usuario_existente = db.query(UsuarioDB).filter(UsuarioDB.email == usuario.email).first()
+    usuario_existente = (
+        db.query(UsuarioDB).filter(UsuarioDB.email == usuario.email).first()
+    )
     if usuario_existente:
         raise HTTPException(status_code=400, detail="Ese email ya está registrado")
 
     nuevo_usuario = UsuarioDB(
-        email=usuario.email,
-        password_hash=hashear_password(usuario.password)
+        email=usuario.email, password_hash=hashear_password(usuario.password)
     )
     db.add(nuevo_usuario)
     db.commit()
     db.refresh(nuevo_usuario)
     return {"mensaje": "Usuario registrado con éxito", "email": nuevo_usuario.email}
 
-@app.post("/login")
-def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    usuario_db = db.query(UsuarioDB).filter(UsuarioDB.email == form_data.username).first()
 
-    if usuario_db is None or not verificar_password(form_data.password, usuario_db.password_hash):
+@app.post("/login")
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+):
+    usuario_db = (
+        db.query(UsuarioDB).filter(UsuarioDB.email == form_data.username).first()
+    )
+
+    if usuario_db is None or not verificar_password(
+        form_data.password, usuario_db.password_hash
+    ):
         raise HTTPException(status_code=401, detail="Email o contraseña incorrectos")
 
     token = crear_token(usuario_db.email)
